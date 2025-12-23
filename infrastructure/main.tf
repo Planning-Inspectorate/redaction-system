c389a1cd20a8d8fc72fc6072733a72f3e5255b2b
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-12-23 13:09:17 +0000

    Add engineers group role assignment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,14 @@
 # data "azurerm_subscription" "current" {}
 
 ############################################################################
+# User groups
+############################################################################
+data "azuread_group" "redaction_engineers" {
+  display_name = "pins-redaction-system-developers"
+  #security_enabled = true
+}
+
+############################################################################
 # Create resource groups
 ############################################################################
 resource "azurerm_resource_group" "redaction_rg" {
@@ -91,4 +99,10 @@ resource "azurerm_role_assignment" "function_app_storage_contributor" {
   scope                = azurerm_storage_account.redaction_storage.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_linux_function_app.redaction_system.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "engineer_storage_contributor" {
+  scope                = azurerm_storage_account.redaction_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azuread_group.redaction_engineers.object_id
 }
