6658c9d16ea393143e83aa2b5424adafada981ee
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-01-06 17:27:48 +0000

    Add redact function

--- redactor/function_app.py
+++ redactor/function_app.py
@@ -5,7 +5,15 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="api", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="redact", methods=["POST"], auth_level=func.AuthLevel.FUNCTION)
+def redact(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200
+    )
+
+
+@app.route(route="api", auth_level=func.AuthLevel.FUNCTION)
 def api(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
