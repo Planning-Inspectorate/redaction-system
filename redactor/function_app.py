b46c08d3f0873e999081d0ef1e5f06cc5f2a4a11
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-01-08 15:28:14 +0000

    Update function names

--- redactor/function_app.py
+++ redactor/function_app.py
@@ -3,29 +3,30 @@ import azure.functions as func
 import azure.durable_functions as df
 import json
 from uuid import uuid4
+import time
+from typing import Dict, Any
 
+# Based on https://learn.microsoft.com/en-us/azure/azure-functions/durable/quickstart-python-vscode?tabs=macos
 app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
 # An HTTP-triggered function with a Durable Functions client binding
 @app.route(route="orchestrators/{functionName}")
 @app.durable_client_input(client_name="client")
-async def http_start(req: func.HttpRequest, client):
+async def http_start(req: func.HttpRequest, client: df.DurableOrchestrationClient):
     function_name = req.route_params.get('functionName')
-    instance_id = await client.start_new(function_name)
-    response = client.create_check_status_response(req, instance_id)
+    run_id = await client.start_new(function_name, client_input=req.params)
+    response = client.create_check_status_response(req, run_id)
     return response
 
 # Orchestrator
 @app.orchestration_trigger(context_name="context")
-def hello_orchestrator(context):
-    result1 = yield context.call_activity("hello", "Seattle")
-    result2 = yield context.call_activity("hello", "Tokyo")
-    result3 = yield context.call_activity("hello", "London")
-
-    return [result1, result2, result3]
+def redact_orchestrator(context: df.DurableOrchestrationContext):
+    result = yield context.call_activity("redact", context.get_input())
+    return [result]
 
 # Activity
-@app.activity_trigger(input_name="city")
-def hello(city: str):
-    return f"Hello {city}"
+@app.activity_trigger(input_name="params")
+def redact(params: Dict[str, Any]):
+    time.sleep(30)
+    return params
