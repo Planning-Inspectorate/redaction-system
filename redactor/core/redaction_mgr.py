ce78a00fd46c634a02e4c0040e773392dc65ae4b
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-01-08 22:35:00 +0000

    Remove debug module

--- redactor/core/redaction_mgr.py
+++
@@ -1,2 +0,0 @@
-def some_function(params):
-    print(f"some_function called with {params}")
--- redactor/function_app.py
+++ redactor/function_app.py
@@ -1,6 +1,5 @@
 import azure.functions as func
-#from core.redaction_manager import RedactionManager
-from core.redaction_mgr import some_function
+from core.redaction_manager import RedactionManager
 import azure.durable_functions as df
 from typing import Dict, Any
 
@@ -54,5 +53,4 @@ def redact_task(params: Dict[str, Any]):
     Task which completes the redaction process
     """
     job_id = params.pop("job_id")
-    return some_function(params)
-    #return RedactionManager(job_id).try_redact(params)
+    return RedactionManager(job_id).try_redact(params)
