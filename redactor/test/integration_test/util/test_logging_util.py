285d0c4ea0a7badbd78309a5f61df95f099cd623
Shannon Williams <shannon.williams.f4@planninginspectorate.gov.uk>
2026-01-14 17:51:56 +0000

    Ruff formatting

--- redactor/test/integration_test/util/test_logging_util.py
+++ redactor/test/integration_test/util/test_logging_util.py
@@ -31,7 +31,6 @@ def app_ins_traces_contains_message(expected_message: str):
     return resp_json.get("tables", [dict()])[0].get("rows", [])
 
 
-
 @pytest.fixture(autouse=True, scope="module")
 @patch("core.util.logging_util.uuid4", return_value=JOB_ID)
 def run_logging_util(mock_job_id):
@@ -45,7 +44,9 @@ def run_logging_util(mock_job_id):
     time.sleep(60)
 
 
-@pytest.mark.flaky(reruns=3, reruns_delay=20, only_rerun="AssertionError") # Flaky test due to delay in logs appearing in app insights
+@pytest.mark.flaky(
+    reruns=3, reruns_delay=20, only_rerun="AssertionError"
+)  # Flaky test due to delay in logs appearing in app insights
 def test__logging_util__logging_initialised():
     expected_logging_initialised_message = (
         f"{JOB_ID}: Logging initialised for redactor_logs."
@@ -58,13 +59,18 @@ def test__logging_util__logging_initialised():
         f"Logging initialisation message not found for job with id {JOB_ID}."
     )
 
-@pytest.mark.flaky(reruns=3, reruns_delay=20, only_rerun="AssertionError") # Flaky test due to delay in logs appearing in app insights
+
+@pytest.mark.flaky(
+    reruns=3, reruns_delay=20, only_rerun="AssertionError"
+)  # Flaky test due to delay in logs appearing in app insights
 def test__logging_util__logging_function_call():
     expected_logging_function_message = (
         f"{JOB_ID}: Function some_test_function called with args: 'Hello', "
         "mock_arg_b='There'"
     )
-    function_call_traces = app_ins_traces_contains_message(expected_logging_function_message)
+    function_call_traces = app_ins_traces_contains_message(
+        expected_logging_function_message
+    )
 
     assert function_call_traces, (
         f"Logging function call message not found for job with id {JOB_ID}."
