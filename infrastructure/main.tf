3da7816440ec66b21df29bc53e64a36286f46f82
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-02-18 14:34:59 +0000

    feat(infrastructure): adding log query alert for log analytics workspace and action group (#59)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -122,7 +122,7 @@ resource "azurerm_linux_function_app" "redaction_system" {
 }
 
 ############################################################################
-# Create App Insights
+# Create App Insights / Monitoring resources
 ############################################################################
 resource "azurerm_log_analytics_workspace" "redaction_system" {
   name                = "${local.org}-log-${local.resource_suffix}"
@@ -144,6 +144,54 @@ resource "azurerm_application_insights" "redaction_system" {
   workspace_id        = azurerm_log_analytics_workspace.redaction_system.id
 
   tags = local.tags
+}
+
+resource "azurerm_monitor_action_group" "redaction_tech" {
+  name                = "pins-ag-redaction-tech-${var.environment}"
+  resource_group_name = azurerm_resource_group.primary.name
+  short_name          = "redaction" # needs to be under 12 characters
+  tags                = local.tags
+
+  # we set emails in the action groups in Azure Portal - to avoid needing to manage emails in terraform
+  lifecycle {
+    ignore_changes = [
+      email_receiver
+    ]
+  }
+}
+
+# Log cap alert using scheduled query rules
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "log_cap" {
+  count = var.environment == "prod" ? 1 : 0
+
+  name         = "Log cap Alert"
+  display_name = "log Daily data limit reached"
+  description  = "Triggered when the log Data cap is reached."
+
+  location            = local.location
+  resource_group_name = azurerm_resource_group.primary.name
+  scopes              = [azurerm_log_analytics_workspace.redaction_system.id]
+
+  enabled                 = true
+  auto_mitigation_enabled = false
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+
+  criteria {
+    query                   = <<-QUERY
+      _LogOperation
+      | where Category =~ "Ingestion" | where Detail contains "OverQuota"
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+  }
+
+  severity = 2
+  action {
+    action_groups = [azurerm_monitor_action_group.redaction_tech.id]
+  }
 }
 
 ############################################################################
