00da3347667e0f73e6e03c58c52cfb5a60704589
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-03-09 11:33:23 +0000

    Improve logging

--- redactor/core/util/azure_vision_util.py
+++ redactor/core/util/azure_vision_util.py
@@ -74,7 +74,7 @@ class AzureVisionUtil:
         ),
         wait=wait_random_exponential(min=1, max=60),
         stop=stop_after_attempt(10),
-        before_sleep=lambda retry_state: LoggingUtil().log_info("Retrying..."),
+        before_sleep=lambda retry_state: LoggingUtil().log_info("Retrying image face detection..."),
         retry_error_callback=handle_last_retry_error,
     )
     def detect_faces(
@@ -169,7 +169,7 @@ class AzureVisionUtil:
         ),
         wait=wait_random_exponential(min=1, max=60),
         stop=stop_after_attempt(10),
-        before_sleep=lambda retry_state: LoggingUtil().log_info("Retrying..."),
+        before_sleep=lambda retry_state: LoggingUtil().log_info("Retrying image text detection..."),
         retry_error_callback=handle_last_retry_error,
     )
     def detect_text(
