8720c496125d25a4d1456172bb1699e9ce5097f3
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-03-19 16:16:43 +0000

    Remove lifecycle block

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -38,10 +38,6 @@ resource "azurerm_storage_account" "redaction_storage" {
     }
   }
 
-  lifecycle {
-    prevent_destroy = false
-  }
-
   sas_policy {
     expiration_period = "01.12:00:00"
   }
