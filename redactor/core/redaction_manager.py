7b3e72d24b8f3f71e6a09b0d9238b270df22f35c
hnikolov <hristo.nikolov@solirus.com>
2026-03-25 12:12:03 +0000

    Reverting unwanted change

--- redactor/core/redaction_manager.py
+++ redactor/core/redaction_manager.py
@@ -353,7 +353,7 @@ class RedactionManager:
             proposed_candidates = [
                 {k: v for k, v in ann.items() if k in attrs_to_compare}
                 for ann in proposed_annots_on_page
-                if ann.get("isRedactionCandidate", True)
+                if ann.get("isRedactionCandidate", False)
             ]
             n_proposed_redactions += len(proposed_candidates)
 
