28e32a47f2d2277d1fc8d50528e93f93506316d8
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-03-26 16:26:52 +0000

    Disable pulbic networking

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -142,17 +142,6 @@ resource "azurerm_service_plan" "processor" {
   #zone_balancing_enabled = true
 }
 
-resource "azurerm_service_plan" "redaction_system_copy" {
-  #checkov:skip=CKV_AZURE_212: TODO: Limit reached in subscription
-  #checkov:skip=CKV_AZURE_225: TODO: Limit reached in subscription
-  name                = "${local.org}-asp-${local.resource_suffix}-copy"
-  resource_group_name = azurerm_resource_group.primary.name
-  location            = local.location
-  os_type             = "Linux"
-  sku_name            = "P2v3"
-  #worker_count           = 2
-  #zone_balancing_enabled = true
-}
 
 resource "azurerm_linux_function_app" "redaction_system" {
   name                = "${local.org}-func-${local.resource_suffix}"
@@ -162,9 +151,9 @@ resource "azurerm_linux_function_app" "redaction_system" {
   storage_account_name          = azurerm_storage_account.redaction_storage.name
   storage_account_access_key    = azurerm_storage_account.redaction_storage.primary_access_key
   service_plan_id               = azurerm_service_plan.processor.id
-  public_network_access_enabled = true
-  #virtual_network_subnet_id     = azurerm_subnet.function_app.id
-  https_only = true
+  public_network_access_enabled = false
+  virtual_network_subnet_id     = azurerm_subnet.function_app.id
+  https_only                    = true
 
   site_config {
     application_stack {
